f16b7d06dc9ec9b12649b295e85606447511d03e
Kostas Kiousis <kkiousis24@gmail.com>
2026-09-09 15:37:54 +0300

    Polish premium doctor profile modal

--- findoc-client/src/main.tsx
+++ findoc-client/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./FilterBubbles.css";
 import "./DoctorCards.css";
+import "./ProfileModal.css";
 
 import App from "./App.tsx";
 import SmartSearchChat from "./SmartSearchChat.tsx";
